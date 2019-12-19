a58a1bdaa89b8d191af239bea81b480d6628e801
Tommy200907 <48202076+Tommy200907@users.noreply.github.com>
2019-12-19 12:03:36 +0100

    new version

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace MyTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World Version2!");
         }
     }
 }
